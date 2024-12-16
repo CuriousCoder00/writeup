75b8128e6ce1eedf3c448f0b410f35d0deb5ef77
Kapil Jangid <kapiljangid040@gmail.com>
2024-12-17 00:29:29 +0530

    Enhance signup form: add loading state, toast notifications, and password match validation

--- app/src/components/auth/signup-form.tsx
+++ app/src/components/auth/signup-form.tsx
@@ -8,7 +8,12 @@ import AuthForm from "./auth-form";
 import { AuthInput } from "./auth-input";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router";
+import { useState } from "react";
+import { useToast } from "@/hooks/use-toast";
+import { Loader } from "lucide-react";
 const SignupForm = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const { toast } = useToast();
   const form = useForm<UserSignupInput>({
     resolver: zodResolver(userSignupSchema),
     defaultValues: {
@@ -18,25 +23,59 @@ const SignupForm = () => {
       confirmPassword: "",
     },
   });
+
+  const signup = async (data: UserSignupInput) => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        "http://localhost:3000/api/v1/auth/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+      const result = await response.json();
+      toast({
+        title: result.message,
+        variant: response.ok ? "default" : "destructive",
+      });
+    } catch (error) {
+      toast({
+        title: (error as Error).message,
+        variant: "destructive",
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <AuthForm form={form}>
-      <form action="" className="flex flex-col w-full gap-4">
+      <form
+        className="flex flex-col w-full gap-4"
+        onSubmit={form.handleSubmit(signup)}
+      >
         <AuthInput
           form={form}
           label="Full Name"
           name="name"
+          disabled={loading}
           placeholder="John Doe"
         />
         <AuthInput
           form={form}
           label="Email Address"
           name="email"
+          disabled={loading}
           placeholder="john.doe@gmail.com"
         />
         <AuthInput
           form={form}
           label="Password"
           name="password"
+          disabled={loading}
           placeholder="******"
           type="password"
         />
@@ -44,11 +83,12 @@ const SignupForm = () => {
           form={form}
           label="Confirm Password"
           name="confirmPassword"
+          disabled={loading}
           placeholder="******"
           type="password"
         />
-        <Button type="submit" className="w-full">
-          Signup
+        <Button disabled={loading} type="submit" className="w-full">
+          {loading ? <Loader className="animate-spin" /> : "Register"}
         </Button>
         <div>
           <p className="text-sm text-end">
--- app/src/lib/validations/user.validator.ts
+++ app/src/lib/validations/user.validator.ts
@@ -8,9 +8,12 @@ export const userLoginSchema = z.object({
 export const userSignupSchema = z.object({
     name: z.string().min(6, "Name must be at least 6 characters"),
     email: z.string().email("Invalid email address"),
-    password: z.string().min(6, "Password must be at least 6 characters"),
-    confirmPassword: z.string().min(6, "Confirm password must be at least 6 characters"),
-})
+    password: z.string().min(6, "Password should be of at least 6 characters"),
+    confirmPassword: z.string().min(6, "Password should be of at least 6 characters")
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+});
 
 export type UserLoginInput = z.infer<typeof userLoginSchema>
 export type UserSignupInput = z.infer<typeof userSignupSchema>
